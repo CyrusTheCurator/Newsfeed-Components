5b7b1de206a3cd559cc2f316abf82bcd4ae4b421
whoIsJosh_ <joshua.c.luscombe@gmail.com>
2020-01-21 00:07:34 -0800

    completed articles js basics and added array population method for menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,12 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
 
 /* 
@@ -33,3 +33,22 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+let header = document.querySelector(".header");
+let menuCreator = items => {
+  let menu = document.createElement("div");
+  let ul = document.createElement("ul");
+  menu.appendChild(ul);
+
+  menu.classList.add("menu");
+
+  items.forEach(menuString => {
+    let menuItem = document.createElement("li");
+    menuItem.textContent = menuString;
+    ul.appendChild(menuItem);
+  });
+
+  console.log(ul);
+  return ul;
+};
+
+header.prepend(menuCreator(menuItems));
